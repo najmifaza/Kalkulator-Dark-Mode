04927d52f4b1cac337d0086a33baa37e86bc7622
Adridinan Najmi Faza <104964855+najmifaza@users.noreply.github.com>
2025-12-03 15:00:55 +0700

    Enable prompt for user name and add VSCode settings
    Uncommented the prompt and alert for user name in script-calc.js to enhance user interaction. Added .vscode/settings.json to specify Live Server port for development environment.

--- script-calc.js
+++ script-calc.js
@@ -1,24 +1,25 @@
 //Tambahin BOM biar asik
-// let nama = prompt('Nama Kamu Siapa');
-// alert(`Halo, ${nama}👋🏻 Mau Ngitung yaaa? btw ada fitur dark mode loh, Silakan menekan OK di bawah untuk memunculkan Kalkulator`);
+let nama = prompt("Nama Kamu Siapa");
+alert(
+  `Halo, ${nama}👋🏻 Mau Ngitung yaaa? btw ada fitur dark mode loh, Silakan menekan OK di bawah untuk memunculkan Kalkulator`
+);
 
 //Fitur Utama
-function appendValue(value) { 
-    document.getElementById("display").value += value;
+function appendValue(value) {
+  document.getElementById("display").value += value;
 }
 
 function clearDisplay() {
-    document.getElementById("display").value = '';
+  document.getElementById("display").value = "";
 }
 
 function calculate() {
-    const display = document.getElementById("display");
-    try {
-        console.log('calculating', display.value);
-        display.value = eval(display.value); 
-    } catch (e) { 
-        console.error('eror! value tidak sesuai');
-        display.value = 'YANG BENER NYED!';
-    }
+  const display = document.getElementById("display");
+  try {
+    console.log("calculating", display.value);
+    display.value = eval(display.value);
+  } catch (e) {
+    console.error("eror! value tidak sesuai");
+    display.value = "YANG BENER NYED!";
+  }
 }
-
